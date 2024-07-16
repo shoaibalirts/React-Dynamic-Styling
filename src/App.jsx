0176a1d78a4cdaec50a90f857179b1e047ea22bb
shoaibalirts <shoaibali.rts@gmail.com>
2024-07-16 18:48:00 +0200

    added css active class

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 import { useState } from "react";
 
-function App() {}
+function App() {
+  const [isClass, setIsClass] = useState(false);
+  function clickHandle() {
+    setIsClass((prevState) => !prevState);
+  }
+  return (
+    <div>
+      <p className={isClass === true ? "active" : undefined}>Style me!</p>
+      <button onClick={clickHandle}>Toggle style</button>
+    </div>
+  );
+}
 
 export default App;
